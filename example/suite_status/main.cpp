3b7522bf5796469ae05d5ddb8c217db58a88e087
perillaroc <perillaroc@gmail.com>
2019-03-04 13:52:25 +0800

    :art: change tab space in cmakelists.

--- example/suite_status/main.cpp
+++ example/suite_status/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <ecflow_client.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     EcflowUtil::EcflowClient client{"10.40.143.18", "31071"};
     client.sync();
     auto bunch = client.bunch();
 
     for(auto &node: bunch->children()){
         std::cout<<node->nodePath()<<" status: "
-        <<WorkflowModel::nodeStatusToString(node->status())<<std::endl;
+            <<WorkflowModel::nodeStatusToString(node->status())
+            <<std::endl;
     }
 
     return 0;
