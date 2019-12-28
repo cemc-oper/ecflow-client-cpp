4852ea59eb62fb998903c4c5d9d77ac5abab5f69
perillaroc <perillaroc@gmail.com>
2019-12-28 18:18:14 +0800

    :construction: add a simple example to test memory cost.

--- src/ecflow_watchman/src/main.cpp
+++ src/ecflow_watchman/src/main.cpp
@@ -1,7 +1,22 @@
+#include <ecflow_client/ecflow_client.h>
+
 #include <spdlog/spdlog.h>
 #include <CLIUtils/CLI11.hpp>
 
 #include <sstream>
+#include <thread>
+
+void runTask() {
+    using namespace std::chrono_literals;
+    while(true) {
+        EcflowUtil::EcflowClient client{"10.40.143.18", "31071"};
+        client.sync();
+        auto records = client.statusRecords();
+        spdlog::info("get nodes...{}", records.size());
+
+        std::this_thread::sleep_for(10s);
+    }
+}
 
 
 int main(int argc, char **argv) {
@@ -10,6 +25,8 @@ int main(int argc, char **argv) {
     std::string rpc_target;
 
     CLI11_PARSE(app, argc, argv)
+
+    runTask();
 
     return 0;
 }
